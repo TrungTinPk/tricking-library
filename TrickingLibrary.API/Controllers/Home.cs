35e98762b3c363336044228b5919d91788ffe292
Kevin Tran <tinttran@kpmg.com.vn>
2020-10-30 15:24:54 +0700

    first commit

---
+++ TrickingLibrary.API/Controllers/Home.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello world";
+        }
+        
+    }
+}
